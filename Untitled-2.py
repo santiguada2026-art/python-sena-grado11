18fb40e5651467adf6e643563cf9021126ce6337
santiguada2026-art <santiguada2026@gmail.com>
2026-07-09 16:39:45 -0500

    Agregar ejercicios de Python

---
+++ Untitled-2.py
@@ -0,0 +1,7 @@
+p1=int(input("ingrese el valor del primer producto"))
+p2=int(input("ingrese el valor del segundo producto"))
+p3=int(input("ingrese el valor del tercer producto"))
+suma=p1+p2+p3
+i=suma*19/100
+iva=suma+i
+print("el valor total es:",iva)
